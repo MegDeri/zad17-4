bb7341475808701d0ab42f37ec72ed6c222ac149
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2020-03-06 18:27:51 +0200

    Fix error in timer

--- modules/time.js
+++ modules/time.js
@@ -1,14 +1,15 @@
-var os = require('os');
+var os = require("os");
 
 function convertTime(num) {
+  var hour = Math.floor(num / 3600);
+  var minute = Math.floor((num % 3600) / 60);
+  var seconds = Math.floor((num % 3600) % 60);
+  var hShow = hour > 0 ? hour + (hour == 1 ? " hour, " : " hours, ") : "";
+  var minShow =
+    minute > 0 ? minute + (minute == 1 ? " minute, " : " minutes, ") : "";
+  var secShow =
+    seconds > 0 ? seconds + (seconds == 1 ? " second" : " seconds") : "";
+  return hShow + minShow + secShow;
+}
 
-    var hour = Math.floor(num / 3600);
-    var minute = Math.floor(num % 3600 / 60);
-    var seconds = Math.floor(num % 3600 / 60);
-    var hShow = hour > 0 ? hour + (hour == 1 ? " hour, " : " hours, ") : "";
-    var minShow = minute > 0 ? minute + (minute == 1 ? " minute, " : " minutes, ") : "";
-    var secShow = seconds > 0 ? seconds + (seconds == 1 ? " second" : " seconds") : "";
-    return hShow + minShow + secShow; 
-};
-
-  exports.print2 = convertTime;
+exports.print2 = convertTime;
